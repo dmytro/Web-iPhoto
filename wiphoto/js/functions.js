562a9f5eaa873611f4025cec9c13615a840c24a6
Dmytro Kovalov <dmytro.kovalov@gmail.com>
2009-12-03 11:55:57 +0900

    merge photo funtions into wiphoto.photo object

--- wiphoto/js/functions.js
+++ wiphoto/js/functions.js
@@ -20,20 +20,21 @@ wiphoto = {
         'photo':
         '<CENTER><IMG WIDTH="${dim[0]}" HEIGHT="${dim[1]}" NAME="SlideShow" ID="showPhoto" SRC="${photos[key]["image"]["path"]}"></CENTER>'
     },
-    album:0, 
+//    album:0, 
     thumb_size: wiphoto.defaults.thumb_size,
     mode: function(mode) {
         if(mode) {
             wiphoto._mode = mode;
             switch (mode) {
             case 'photo':
-            case 'slide': 
             case 'thumb': 
+            case 'slide': 
                 next = wiphoto.photo.next; 
                 prev = wiphoto.photo.prev; 
                 show = wiphoto.photo.show; 
                 play = slideShow.play;
                 break;
+
             case 'albumThumb':
                 next = false;
                 prev = false;
@@ -45,176 +46,183 @@ wiphoto = {
         return (wiphoto._mode)
     },
     // ------------------------------------------------------------
+    album: {
+        current: 0,
+        selected: 0
+    },
+    // ------------------------------------------------------------
     photo: {
         current: 0,
-        show: function (album,index) {
+        show: function (_album,index) {
             wiphoto.photo.current = (index==0) ?0 :(index||wiphoto.photo.current)
-            wiphoto.album = album || wiphoto.album
+            wiphoto.album.current = _album || wiphoto.album.current
 	    w = $('viewPanel').clientWidth -20 
 	    h = $('viewPanel').clientHeight -20
-            key = albums[wiphoto.album]['photos'][wiphoto.photo.current];
+            key = albums[wiphoto.album.current]['photos'][wiphoto.photo.current];
 	    dim = fitInto(w,h,photos[key]['image']);
  	    $('viewPanel').innerHTML = TrimPath.parseTemplate(wiphoto.template.photo).process({'dim':dim, 'key':key, 'photos':photos})
         },
         // --------------------------------------------------------------------------------
-        last:    function () { return (albums[wiphoto.album].photos.length-1) },
-        next:    function () { return ( (wiphoto.photo.current == wiphoto.photo.last()) ? 0 :wiphoto.photo.current+1 ) },
-        prev:    function () { return ( (wiphoto.photo.current == 0) ? wiphoto.photo.last():wiphoto.photo.current-1 ) },
-        setmode: function(album,index)  { wiphoto.mode('photo'); show(album,index) }
+        last:    function () { return (albums[wiphoto.album.current].photos.length-1) },
+        next:    function () { with(wiphoto.photo) {return ( (current == last()) ? 0 :current+1 ) }},
+        prev:    function () { with(wiphoto.photo) {return ( (current == 0) ? last():current-1 ) }},
+        setmode: function(_album,index)  { wiphoto.mode('photo'); show(_album,index) }
     },
+    // ------------------------------------------------------------
     preload: {},
-
     // ------------------------------------------------------------
 }
 
-keyCode = { 32:'space',33:'pgup',34:'pgdown',35:'end',36:'home',37:'left',38:'up',39:'right',40:'down',27:'escape',9:'tab',13:'enter',48:'0',
-            83:'s'
-          };
+    keyCode = { 32:'space',33:'pgup',34:'pgdown',35:'end',36:'home',37:'left',38:'up',39:'right',40:'down',27:'escape',9:'tab',13:'enter',48:'0',
+                83:'s'
+              };
 
-    
-//     current = { 
-//         photo:0, album:0, thumb_size:wiphoto.defaults.thumb_size,
-//                 mode:function(mode) {
-//                     if(mode) { 
-//                         current._mode = mode;
-                        
-//                         if(mode != 'slide') slideShow.stop()
-//                     }
-//                     return (current._mode)
-//                 },
-//               };
+              
+              //     current = { 
+              //         photo:0, album:0, thumb_size:wiphoto.defaults.thumb_size,
+              //                 mode:function(mode) {
+              //                     if(mode) { 
+              //                         current._mode = mode;
+              
+              //                         if(mode != 'slide') slideShow.stop()
+              //                     }
+              //                     return (current._mode)
+              //                 },
+              //               };
 
-    // --------------------------------------------------------------------------------
-    function getElementsByClassName(classname, node)  {
-        if(!node) node = document.getElementsByTagName("body")[0];
-        var a = [];
-        var re = new RegExp('\\b' + classname + '\\b');
-        var els = node.getElementsByTagName("*");
-        for(var i=0,j=els.length; i<j; i++)
-            if(re.test(els[i].className))a.push(els[i]);
-        return a;
-    }
-    function get1stElementByClassName(classname, node)  {
-        return (getElementsByClassName(classname, node)[0])
-    }
-    // --------------------------------------------------------------------------------
-    function setThumbSelection (album,thumb) {
-        elem = $('thumb_'+album+'_'+thumb);
-        setSelection(elem);
-        if (wiphoto.mode('thumb')) { wiphoto.photo.current = thumb }
-    }
-    // --------------------------------------------------------------------------------
-    function setSelection (elem,cls) {
-        cls = cls || wiphoto.mode();
-        if (elem_s = get1stElementByClassName(cls+' selected',document.body)) { elem_s.className = cls }
-        elem.addClassName('selected');
-        elem.scrollIntoView(false);
-    }
-    // --------------------------------------------------------------------------------
-    function showAlbumThumbs () {
-        wiphoto.mode('albumThumb');
-        template = wiphoto.template;
-	out = '';
-        $('viewPanel').innerHTML = '';
-	for (var album in albums) {
-    	    out += TrimPath.parseTemplate(template['albumLink']).process({'album': album, 'albums': albums});
-            dim = fitInto(wiphoto['thumb_size'], wiphoto['thumb_size'], photos[albums[album]['photos'][0]]['thumb']);
-            $('viewPanel').innerHTML += 
-            TrimPath.parseTemplate(template['albumThumb']).process({'wiphoto':wiphoto,'photos':photos,'dim':dim,'album': album, 'albums': albums});
-	}
-	$('albumdata').innerHTML = out
-    }
+              // --------------------------------------------------------------------------------
+              function getElementsByClassName(classname, node)  {
+                  if(!node) node = document.getElementsByTagName("body")[0];
+                  var a = [];
+                  var re = new RegExp('\\b' + classname + '\\b');
+                  var els = node.getElementsByTagName("*");
+                  for(var i=0,j=els.length; i<j; i++)
+                      if(re.test(els[i].className))a.push(els[i]);
+                  return a;
+              }
+              function get1stElementByClassName(classname, node)  {
+                  return (getElementsByClassName(classname, node)[0])
+              }
+              // --------------------------------------------------------------------------------
+              function setThumbSelection (_album,thumb) {
+                  elem = $('thumb_'+_album+'_'+thumb);
+                  setSelection(elem);
+                  if (wiphoto.mode('thumb')) { wiphoto.photo.current = thumb }
+              }
+              // --------------------------------------------------------------------------------
+              function setSelection (elem,cls) {
+                  cls = cls || wiphoto.mode();
+                  if (elem_s = get1stElementByClassName(cls+' selected',document.body)) { elem_s.className = cls }
+                  elem.addClassName('selected');
+                  elem.scrollIntoView(false);
+              }
+              // --------------------------------------------------------------------------------
+              function showAlbumThumbs () {
+                  wiphoto.mode('albumThumb');
+                  template = wiphoto.template;
+	          out = '';
+                  $('viewPanel').innerHTML = '';
+	          for (var album in albums) {
+    	              out += TrimPath.parseTemplate(template['albumLink']).process({'album': album, 'albums': albums});
+                      dim = fitInto(wiphoto['thumb_size'], wiphoto['thumb_size'], photos[albums[album]['photos'][0]]['thumb']);
+                      $('viewPanel').innerHTML += 
+                      TrimPath.parseTemplate(template['albumThumb']).process({'wiphoto':wiphoto,'photos':photos,'dim':dim,'album': album, 'albums': albums});
+	          }
+	          $('albumdata').innerHTML = out
+              }
 
-    // --------------------------------------------------------------------------------
-    function showThumbs (album,thumb) {
-        wiphoto.mode('thumb');
-        wiphoto.album = album || wiphoto.album;
-        wiphoto.photo.current = (thumb==0) ?0 :(thumb || wiphoto.photo.current); 
+              // --------------------------------------------------------------------------------
+              function showThumbs (_album,thumb) {
+                  wiphoto.mode('thumb');
+                  wiphoto.album.current = _album || wiphoto.album.current;
+                  wiphoto.photo.current = (thumb==0) ?0 :(thumb || wiphoto.photo.current); 
 
-        setSelection( $("showThumbs_"+wiphoto.album), 'albumLink'); // left side tab
+                  setSelection( $("showThumbs_"+wiphoto.album.current), 'albumLink'); // left side tab
 
-	arr = albums[wiphoto.album].photos;	
-	$('viewPanel').innerHTML = '';
-	for (var i=0; i< arr.length; i=i+1) {
-	    key=arr[i];
-            dim = fitInto(wiphoto.thumb_size, wiphoto.thumb_size, photos[key].thumb);
-            template = wiphoto.template;
-	    $('viewPanel').innerHTML +=
-	    TrimPath.parseTemplate(template.thumb).process(
-		{'key': key, 'i': i, 'album': wiphoto.album, 'photos': photos, "albums": albums, 'dim':dim}
-	    )
-	}
-        setThumbSelection(wiphoto.album,wiphoto.photo.current);
-    }
-    // --------------------------------------------------------------------------------
-    function selectAlbum(album) {
-        wiphoto.album = album;
-        setSelection( $("showThumbs_"+wiphoto.album), 'albumLink'); // left side tab
-        setSelection( $("albumThumb_img_"+wiphoto.album), 'albumThumb_img'); 
+	          arr = albums[wiphoto.album.current].photos;	
+	          $('viewPanel').innerHTML = '';
+	          for (var i=0; i< arr.length; i=i+1) {
+	              key=arr[i];
+                      dim = fitInto(wiphoto.thumb_size, wiphoto.thumb_size, photos[key].thumb);
+                      template = wiphoto.template;
+	              $('viewPanel').innerHTML +=
+	              TrimPath.parseTemplate(template.thumb).process(
+		          {'key': key, 'i': i, 'album': wiphoto.album.current, 'photos': photos, "albums": albums, 'dim':dim}
+	              )
+	          }
+                  setThumbSelection(wiphoto.album.current,wiphoto.photo.current);
+              }
+              // --------------------------------------------------------------------------------
+              function selectAlbum(_album) {
+                  wiphoto.album.current = _album;
+                  setSelection( $("showThumbs_"+wiphoto.album.current), 'albumLink'); // left side tab
+                  setSelection( $("albumThumb_img_"+wiphoto.album.current), 'albumThumb_img'); 
 
-    }
-    
-    function fitInto (w,h,image) { // Container width and height, photo width and height
-        proportion = Math.max(image['dim'][0]/w,image['dim'][1]/h);
-	return([image['dim'][0]/proportion, image['dim'][1]/proportion])
-    }
-    
-    // --------------------------------------------------------------------------------
+              }
+              
+              function fitInto (w,h,image) { // Container width and height, photo width and height
+                  proportion = Math.max(image['dim'][0]/w,image['dim'][1]/h);
+	          return([image['dim'][0]/proportion, image['dim'][1]/proportion])
+              }
+              
+              // --------------------------------------------------------------------------------
 
-    function KeyCheck (e) {
-        var KeyPress = (window.event) ? keyCode[event.keyCode] : keyCode[e.keyCode];
-        switch (wiphoto.mode()) {
-        case 'albumThumb':
-            switch(KeyPress) {
-            case 'enter': 
-            case 's': 
-                slideShow.play(); break;
-            case 'space': showThumbs(); break;
-            }
-            break
-        case 'photo':
-            switch(KeyPress) {
-            case 'left': show(null,prev()); break;
-            case 'right': 
-            case 'space':  show(null,next()); break;
-            case 'escape': showThumbs(); break;
-            case 'enter': slideShow.play(); break;
-            }
-            break
-        case 'thumb':
-            switch(KeyPress) {
-            case 'pgup': 
-            case '0':      setThumbSelection(wiphoto.album,0); break;
-            case 'right':  setThumbSelection(wiphoto.album, next()); break;
-            case 'left':   setThumbSelection(wiphoto.album, prev()); break;
-            case 'pgdown': setThumbSelection(wiphoto.album, last()); break;
+              function KeyCheck (e) {
+                  var KeyPress = (window.event) ? keyCode[event.keyCode] : keyCode[e.keyCode];
+                  switch (wiphoto.mode()) {
+                  case 'albumThumb':
+                      switch(KeyPress) {
+                      case 'enter': 
+                      case 's': 
+                          slideShow.play(); break;
+                      case 'space': showThumbs(); break;
+                      }
+                      break
+                  case 'photo':
+                      switch(KeyPress) {
+                      case 'left': show(null,prev()); break;
+                      case 'right': 
+                      case 'space':  show(null,next()); break;
+                      case 'escape': showThumbs(); break;
+                      case 'enter': slideShow.play(); break;
+                      }
+                      break
+                  case 'thumb':
+                      switch(KeyPress) {
+                      case 'pgup': 
+                      case '0':      setThumbSelection(null, 0); break;
+                      case 'right':  setThumbSelection(null, next()); break;
+                      case 'left':   setThumbSelection(null, prev()); break;
+                      case 'pgdown': setThumbSelection(null, last()); break;
 
-            case 'space': wiphoto.photo.mode();break;
-            case 'enter': slideShow.play(); break;
+                      case 'space': wiphoto.photo.setmode();break;
+                      case 'enter': slideShow.play(); break;
 
-            case 'escape': showAlbumThumbs(); break;
-            }
-            break
-        case 'slide':
-            switch(KeyPress) {
-            case 'escape': 
-                if (slideShow.running) slideShow.stop()
-                else showThumbs()
-                break;
-            case 'space':
-            case 'enter': slideShow.toggle(); break;
-            case 'right': slideShow.next(); break;
-            case 'left':  slideShow.prev(); break;
-            }
-        }
-        return (true);
-    }
-    
+                      case 'escape': showAlbumThumbs(); break;
+                      }
+                      break
+                  case 'slide':
+                      with (slideShow) {
+                          switch(KeyPress) {
+                          case 'escape': 
+                              if (running) stop()
+                              else showThumbs()
+                              break;
+                          case 'space':
+                          case 'enter': toggle(); break;
+                          case 'right': show(null, window.next()); break;
+                          case 'left':  show(null, window.prev()); break;
+                          }
+                      }
+                  }
+                  return (true);
+              }
+              
 
-// --------------------------------------------------------------------------------
-    function init() {
-        showAlbumThumbs();
-        document.onkeyup = KeyCheck;       
-        wiphoto.mode('albumThumb')
-        selectAlbum(1289);
-    }
+              // --------------------------------------------------------------------------------
+              function init() {
+                  showAlbumThumbs();
+                  document.onkeyup = KeyCheck;       
+                  wiphoto.mode('albumThumb')
+                  selectAlbum(1289);
+              }
--- wiphoto/js/slides.js
+++ wiphoto/js/slides.js
@@ -4,64 +4,74 @@ slideShow = {
     timeout: false,
     preloaded: 0,
     speed: wiphoto.defaults.slideShowSpeed,
-    current: 0,
 
-    play: function (album,photo) {
-        wiphoto.album = album || wiphoto.album
+    play: function (_album,photo) {
+        wiphoto.album.current = _album || wiphoto.album.current
         wiphoto.photo.current = (photo == 0) ? 0 : (photo || wiphoto.photo.current)
-        this.slides = albums[wiphoto.album].photos;
-        this.preload(0)
-        this.current = wiphoto.photo.current
-        this.next();
+        with(slideShow) {
+            slides = albums[wiphoto.album.current].photos;
+            preload(0)
+            next();
+        }
     },
     clear: function() {
         clearTimeout(slideShow.timeout)
     },
     stop: function () {
-        slideShow.slides = [];
-        slideShow.clear()
-        slideShow.running = false
+        with(slideShow) {
+            slides = [];
+            clear()
+            running = false
+        }
     },
     toggle: function () {
-        if (slideShow.running) slideShow.stop()
-        else slideShow.play(wiphoto.album,wiphoto.photo.current)
+// TODO
+// file:///Users/dmytro/Development/Web-iPhoto/wiphoto/js/slides.js:12TypeError: Result of expression 'albums[wiphoto.album.current]' [undefined] is not an object.
+        with(slideShow) {
+            if (running) stop()
+            else with(wiphoto) play(album, photo.current)
+        }
     },
     prev: function () {
-        slideShow.current = slideShow.current -2
-        slideShow.next()
+        with(wiphoto.photo) {current = current -2; slideShow.next() }
     },
     next: function (){
-        wiphoto.mode('slide')
-        slideShow.running = true
-        if (slideShow.slides.length == 0) return
-        slideShow.clear()
-        slideShow.preload()
+        with(slideShow) {
+            wiphoto.mode('slide')
+            running = true
+            if (slides.length == 0) return
+            clear()
+            preload()
         
-        //     if (document.all){
-        //         document.images.SlideSlideShow.style.filter="blendTrans(duration=2)"
-        //         document.images.SlideSlideShow.style.filter="blendTrans(duration=crossFadeDuration)"
-        //         document.images.SlideShow.filters.blendTrans.Apply()      
-        //     }
-        wiphoto.photo.show(); wiphoto.photo.current++
-        
-        if (wiphoto.preload[slideShow.slides[slideShow.current]]) 
-            document.images.SlideShow.src = wiphoto.preload[slideShow.slides[slideShow.current]].src
-        else slideShow.preload(slideShow.current)
-
-        if (++slideShow.current > (slideShow.slides.length-1)) slideShow.current = 0
-        slideShow.timeout = setTimeout('slideShow.next()', slideShow.speed*1000)
+            //     if (document.all){
+            //         document.images.SlideSlideShow.style.filter="blendTrans(duration=2)"
+            //         document.images.SlideSlideShow.style.filter="blendTrans(duration=crossFadeDuration)"
+            //         document.images.SlideShow.filters.blendTrans.Apply()      
+            //     }
+            with (wiphoto.photo) { 
+                show(); 
+            
+                if (wiphoto.preload[slides[current]]) 
+                    document.images.SlideShow.src = wiphoto.preload[slides[current]].src
+                else preload(current)
+                
+                if (++current > (slides.length-1)) current = 0
+                timeout = setTimeout('slideShow.next()', speed*1000)
+            }
+        }
     },
     preload: function (startAt) {
-        slideShow.preloaded = (startAt == 0) ? 0 : (startAt || slideShow.preloaded)
-        for (i=0; i<5; i=i+1) {
-            key = slideShow.slides[slideShow.preloaded]
-            if (!wiphoto.preload[key]) { 
-                wiphoto.preload[key] = new Image ()
-                wiphoto.preload[key].src = photos[key].image.path
+        with(slideShow) {
+            preloaded = (startAt == 0) ? 0 : (startAt || preloaded)
+            for (i=0; i<5; i=i+1) {
+                key = slides[preloaded]
+                if (!wiphoto.preload[key]) { 
+                    wiphoto.preload[key] = new Image ()
+                    wiphoto.preload[key].src = photos[key].image.path
+                }
+                preloaded = (preloaded > slides.length-1) ? 0 : preloaded+1
             }
-            slideShow.preloaded = (slideShow.preloaded > slideShow.slides.length-1) ? 0 : slideShow.preloaded+1
         }
-        console.log (slideShow.preloaded)
         
     }
 }
